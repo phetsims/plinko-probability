03135694b44f87c7c367c9b7a7a25f6ea05e4d82
bereaphysics <martin.veillette@gmail.com>
2015-02-24 21:49:53 -0700

    order alphabetically the modules

--- js/common/view/FractionButton.js
+++ js/common/view/FractionButton.js
@@ -10,8 +10,8 @@ define( function( require ) {
   'use strict';
 
   // modules
-  var inherit = require( 'PHET_CORE/inherit' );
   var Image = require( 'SCENERY/nodes/Image' );
+  var inherit = require( 'PHET_CORE/inherit' );
   var RectangularPushButton = require( 'SUN/buttons/RectangularPushButton' );
 
   // images
