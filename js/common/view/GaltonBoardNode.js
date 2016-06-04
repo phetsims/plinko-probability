f94bbd1669e82142f74be65cf8602315594b708a
veillette <martin.veillette@gmail.com>
2016-06-03 21:27:54 -0400

    remove unused module

--- js/common/view/GaltonBoardNode.js
+++ js/common/view/GaltonBoardNode.js
@@ -9,7 +9,6 @@ define( function( require ) {
     'use strict';
 
     // modules
-    //var Bounds2 = require( 'DOT/Bounds2' );
     var plinkoProbability = require( 'PLINKO_PROBABILITY/plinkoProbability' );
     var Circle = require( 'SCENERY/nodes/Circle' );
     var inherit = require( 'PHET_CORE/inherit' );
