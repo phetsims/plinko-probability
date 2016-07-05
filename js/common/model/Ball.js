a8d3ddd1938b7b4e052dd1654a84f646423da7f7
Guillermo <guillermoramos330179@gmail.com>
2016-07-05 10:46:18 -0400

    Add documentation to the JSDoc about the function of step function (see #35)

--- js/common/model/Ball.js
+++ js/common/model/Ball.js
@@ -173,7 +173,13 @@ define( function( require ) {
     },
     /**
      *
-     * Updates the position of the ball
+     * Updates the peg information (rowNumber, columnNumber, and location) used for determining position
+     * check and changes the phase of the ball PHASE_INITAL, PHASE_FALLING, PHASE_EXITED and PHASE_LANDED
+     * Plays a sounds when the ball hits a peg
+     * Emits when the ball has exited so that it can be added to statistics
+     * Emits when the ball has landed so that it doesn't get painted when it doesn't need to
+     * updates the position of the ball
+     *
      * @param {number} df - fraction of falling between pegs
      * @public
      */
