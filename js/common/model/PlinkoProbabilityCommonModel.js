a28de5c0663d1e6fc96be54cac833e0048c97306
Chris Malley <cmalley@pixelzoom.com>
2016-08-26 14:18:51 -0600

    fix formatting

--- js/common/model/PlinkoProbabilityCommonModel.js
+++ js/common/model/PlinkoProbabilityCommonModel.js
@@ -7,61 +7,58 @@
  */
 
 define( function( require ) {
-    'use strict';
+  'use strict';
 
-    // modules
-    var GaltonBoard = require( 'PLINKO_PROBABILITY/common/model/GaltonBoard' );
-    var Histogram = require( 'PLINKO_PROBABILITY/common/model/Histogram' );
-    var inherit = require( 'PHET_CORE/inherit' );
-    var ObservableArray = require( 'AXON/ObservableArray' );
-    var PropertySet = require( 'AXON/PropertySet' );
-    var PlinkoConstants = require( 'PLINKO_PROBABILITY/common/PlinkoConstants' );
-    var plinkoProbability = require( 'PLINKO_PROBABILITY/plinkoProbability' );
+  // modules
+  var GaltonBoard = require( 'PLINKO_PROBABILITY/common/model/GaltonBoard' );
+  var Histogram = require( 'PLINKO_PROBABILITY/common/model/Histogram' );
+  var inherit = require( 'PHET_CORE/inherit' );
+  var ObservableArray = require( 'AXON/ObservableArray' );
+  var PropertySet = require( 'AXON/PropertySet' );
+  var PlinkoConstants = require( 'PLINKO_PROBABILITY/common/PlinkoConstants' );
+  var plinkoProbability = require( 'PLINKO_PROBABILITY/plinkoProbability' );
+
+  /**
+   * Creates common model for Plinko Probability
+   * @constructor
+   */
+  function PlinkoProbabilityCommonModel() {
+
+    // @public
+    PropertySet.call( this, {
+      probability: PlinkoConstants.BINARY_PROBABILITY_RANGE.defaultValue, // {number} this can be a number between 0 and 1
+      histogramMode: 'count', // {string} acceptable values are 'count', 'fraction' and 'cylinder'
+      ballMode: 'oneBall', // {string} acceptable values are 'oneBall', 'tenBalls', 'allBalls' and 'continuous'
+      isBallCapReached: false, // {boolean} is the maximum of balls reached?
+      numberOfRows: PlinkoConstants.ROWS_RANGE.defaultValue /// {number} must be an integer
+    } );
+
+    this.ballCreationTimeElapsed = 0; // @public {number} - time elapsed since last ball creation;
+
+    // create an observable array of the model balls
+    this.balls = new ObservableArray(); // @public
+
+    // create the model for the Galton Board which describes the position of the pegs and their visibility
+    this.galtonBoard = new GaltonBoard( this.numberOfRowsProperty ); // @public
+
+    // create the model for the histogram
+    this.histogram = new Histogram( this.numberOfRowsProperty ); // @public
+  }
+
+  plinkoProbability.register( 'PlinkoProbabilityCommonModel', PlinkoProbabilityCommonModel );
+
+  return inherit( PropertySet, PlinkoProbabilityCommonModel, {
 
     /**
-     * Creates common model for Plinko Probability
-     * @constructor
+     * Reset of the model attributes.
+     * @public
      */
-    function PlinkoProbabilityCommonModel() {
-
-      // @public
-      PropertySet.call( this, {
-        probability: PlinkoConstants.BINARY_PROBABILITY_RANGE.defaultValue, // {number} this can be a number between 0 and 1
-        histogramMode: 'count', // {string} acceptable values are 'count', 'fraction' and 'cylinder'
-        ballMode: 'oneBall', // {string} acceptable values are 'oneBall', 'tenBalls', 'allBalls' and 'continuous'
-        isBallCapReached: false, // {boolean} is the maximum of balls reached?
-        numberOfRows: PlinkoConstants.ROWS_RANGE.defaultValue /// {number} must be an integer
-      } );
-
-      this.ballCreationTimeElapsed = 0; // @public {number} - time elapsed since last ball creation;
-
-      // create an observable array of the model balls
-      this.balls = new ObservableArray(); // @public
-
-      // create the model for the Galton Board which describes the position of the pegs and their visibility
-      this.galtonBoard = new GaltonBoard( this.numberOfRowsProperty ); // @public
-
-      // create the model for the histogram
-      this.histogram = new Histogram( this.numberOfRowsProperty ); // @public
+    reset: function() {
+      PropertySet.prototype.reset.call( this );
+      this.balls.clear(); // clear all the model balls
+      this.histogram.reset(); // empty out all the model bins
+      this.ballCreationTimeElapsed = 0;
     }
-
-    plinkoProbability.register( 'PlinkoProbabilityCommonModel', PlinkoProbabilityCommonModel );
-
-    return inherit( PropertySet, PlinkoProbabilityCommonModel, {
-
-      /**
-       * Reset of the model attributes.
-       * @public
-       */
-      reset: function() {
-        PropertySet.prototype.reset.call( this );
-        this.balls.clear(); // clear all the model balls
-        this.histogram.reset(); // empty out all the model bins
-        this.ballCreationTimeElapsed = 0;
-      }
-
-    } );
-  }
-)
-;
+  } );
+} );
 
