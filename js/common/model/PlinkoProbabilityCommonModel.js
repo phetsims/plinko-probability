69459320212adbfaeeaaf4092f6835c4e92bb77e
Chris Malley <cmalley@pixelzoom.com>
2016-08-26 18:32:28 -0600

    doc

--- js/common/model/PlinkoProbabilityCommonModel.js
+++ js/common/model/PlinkoProbabilityCommonModel.js
@@ -1,7 +1,7 @@
 // Copyright 2014-2016, University of Colorado Boulder
 
 /**
- * Common Model (base type) for Plinko Probability
+ * Common model (base type) for Plinko Probability
  *
  * @author Martin Veillette (Berea College)
  */
