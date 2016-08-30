70250348b6ed62d2ceca1c45c44561fb3b89b9a4
Chris Malley <cmalley@pixelzoom.com>
2016-08-30 08:57:59 -0600

    use ShadedSphereNode for balls, #64

--- js/common/view/BallNode.js
+++ js/common/view/BallNode.js
@@ -9,33 +9,24 @@ define( function( require ) {
   'use strict';
 
   // modules
-  var Circle = require( 'SCENERY/nodes/Circle' );
   var inherit = require( 'PHET_CORE/inherit' );
   var PlinkoProbabilityConstants = require( 'PLINKO_PROBABILITY/common/PlinkoProbabilityConstants' );
   var plinkoProbability = require( 'PLINKO_PROBABILITY/plinkoProbability' );
-  var RadialGradient = require( 'SCENERY/util/RadialGradient' );
+  var ShadedSphereNode = require( 'SCENERY_PHET/ShadedSphereNode' );
 
   /**
    * @param {number} radius - in view coordinates
    * @constructor
    */
   function BallNode( radius ) {
-
-    // gives the ball a specular highlight
-    var gradient = new RadialGradient(
-      -radius * 0.4, -radius * 0.4, 0,  // x0, y0, r0
-      radius * 0.1, -radius * 0.3, radius * 0.6 ) // x1, y1, r1
-      .addColorStop( 0, PlinkoProbabilityConstants.BALL_HIGHLIGHT_COLOR )
-      .addColorStop( 1, PlinkoProbabilityConstants.BALL_COLOR );
-
-    Circle.call( this, radius, {
+    ShadedSphereNode.call( this, 2 * radius, {
+      mainColor: PlinkoProbabilityConstants.BALL_COLOR,
       stroke: PlinkoProbabilityConstants.BALL_COLOR,
-      lineWidth: 0.1 * radius,
-      fill: gradient
+      lineWidth: 0.1 * radius
     } );
   }
 
   plinkoProbability.register( 'BallNode', BallNode );
 
-  return inherit( Circle, BallNode );
+  return inherit( ShadedSphereNode, BallNode );
 } );
