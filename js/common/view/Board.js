aa410cb4ffb0b6206103a374ccf3ec5f82d4260b
Chris Malley <cmalley@pixelzoom.com>
2016-08-30 10:07:44 -0600

    break excessively long lines, especially for gradient color stops

--- js/common/view/Board.js
+++ js/common/view/Board.js
@@ -27,7 +27,6 @@ define( function( require ) {
     Node.call( this );
 
     options = _.extend( {
-      // defaults
       height: 300,
       bottomWidth: 600,
       shadowFill: 'rgb(136,136,136)'
@@ -43,13 +42,29 @@ define( function( require ) {
       .close();
 
     // create the gradient on face of Galton Board
-    var boardRectangleGradient = new LinearGradient( -options.bottomWidth / 2, 0, options.bottomWidth / 2, 0 ).addColorStop( 0.0112, '#FBEFD0' ).addColorStop( 0.1742, '#FADBA2' ).addColorStop( 0.2978, '#FAE3B0' ).addColorStop( 0.5393, '#E8CFA1' ).addColorStop( 0.6573, '#F0D3A1' ).addColorStop( 0.7809, '#FBEED2' ).addColorStop( 0.9607, '#F9E2BA' );
-    var boardPath = new Path( boardShape, { fill: boardRectangleGradient, centerX: 0, top: 0 } );
+    var boardRectangleGradient = new LinearGradient( -options.bottomWidth / 2, 0, options.bottomWidth / 2, 0 )
+      .addColorStop( 0.0112, '#FBEFD0' )
+      .addColorStop( 0.1742, '#FADBA2' )
+      .addColorStop( 0.2978, '#FAE3B0' )
+      .addColorStop( 0.5393, '#E8CFA1' )
+      .addColorStop( 0.6573, '#F0D3A1' )
+      .addColorStop( 0.7809, '#FBEED2' )
+      .addColorStop( 0.9607, '#F9E2BA' );
+    var boardPath = new Path( boardShape, {
+      fill: boardRectangleGradient,
+      centerX: 0,
+      top: 0
+    } );
     this.addChild( boardPath );
 
     // produces the bottom dropped shadow on the Galton Board
-    var boardShadowRectangleGradient = new LinearGradient( options.bottomWidth / 2, options.height, options.bottomWidth / 2, options.height + 10 ).addColorStop( 0.00, options.shadowFill ).addColorStop( 0.20, options.shadowFill ).addColorStop( 1.00, PlinkoProbabilityConstants.BACKGROUND_COLOR );
-    var horizontalRectangle = new Rectangle( boardPath.left + 4, boardPath.bottom, boardPath.width + 8, 10, 5, 50, { fill: boardShadowRectangleGradient } );
+    var boardShadowRectangleGradient = new LinearGradient( options.bottomWidth / 2, options.height, options.bottomWidth / 2, options.height + 10 )
+      .addColorStop( 0.00, options.shadowFill )
+      .addColorStop( 0.20, options.shadowFill )
+      .addColorStop( 1.00, PlinkoProbabilityConstants.BACKGROUND_COLOR );
+    var horizontalRectangle = new Rectangle( boardPath.left + 4, boardPath.bottom, boardPath.width + 8, 10, 5, 50, {
+      fill: boardShadowRectangleGradient
+    } );
     this.addChild( horizontalRectangle );
 
     // create the dropped shadow to the right of the galton board
@@ -58,12 +73,15 @@ define( function( require ) {
       .lineToRelative( 10, 0 )
       .lineToRelative( -options.bottomWidth / 2, -options.height )
       .close();
-    var boardSlantedGradient = new LinearGradient( options.bottomWidth / 4, options.height / 2, options.bottomWidth / 4 + 5, options.height / 2 - 5 ).addColorStop( 0.00, options.shadowFill ).addColorStop( 0.20, options.shadowFill ).addColorStop( 1.00, PlinkoProbabilityConstants.BACKGROUND_COLOR );
-    var slantedShadowPath = new Path( slantedShadowShape, { fill: boardSlantedGradient } );
-
+    var boardSlantedGradient = new LinearGradient( options.bottomWidth / 4, options.height / 2, options.bottomWidth / 4 + 5, options.height / 2 - 5 )
+      .addColorStop( 0.00, options.shadowFill )
+      .addColorStop( 0.20, options.shadowFill )
+      .addColorStop( 1.00, PlinkoProbabilityConstants.BACKGROUND_COLOR );
+    var slantedShadowPath = new Path( slantedShadowShape, {
+      fill: boardSlantedGradient
+    } );
     this.addChild( slantedShadowPath );
 
-    // Pass options through to the parent class.
     this.mutate( options );
   }
 
