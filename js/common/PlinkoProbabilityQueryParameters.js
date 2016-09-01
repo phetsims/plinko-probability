c15bf3d19f8cca3caf212a16f17c012bd8e649a8
Chris Malley <cmalley@pixelzoom.com>
2016-09-01 11:11:54 -0600

    add radix arg to parseInt

--- js/common/PlinkoProbabilityQueryParameters.js
+++ js/common/PlinkoProbabilityQueryParameters.js
@@ -17,10 +17,10 @@ define( function( require ) {
   var PlinkoProbabilityQueryParameters = {
 
     // maximum number of balls that can be in any 1 bin in the Lab screen, e.g. maxBalls=10
-    MAX_BALLS: getQueryParameter( 'maxBalls' ) && parseInt( getQueryParameter( 'maxBalls' ) ),
+    MAX_BALLS: getQueryParameter( 'maxBalls' ) && parseInt( getQueryParameter( 'maxBalls' ), 10 ),
 
     // number of balls to put in the histogram at startup, e.g. populateHistogram=20
-    POPULATE_HISTOGRAM: getQueryParameter( 'populateHistogram' ) && parseInt( getQueryParameter( 'populateHistogram' ) )
+    POPULATE_HISTOGRAM: getQueryParameter( 'populateHistogram' ) && parseInt( getQueryParameter( 'populateHistogram' ), 10 )
   };
 
   plinkoProbability.register( 'PlinkoProbabilityQueryParameters', PlinkoProbabilityQueryParameters );
