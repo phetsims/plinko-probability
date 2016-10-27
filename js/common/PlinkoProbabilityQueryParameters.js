82e0bedc85cff08a2f1223ee90ba13e6e141d1ee
Chris Malley <cmalley@pixelzoom.com>
2016-10-27 11:51:55 -0600

    use isValidValue in sims, https://github.com/phetsims/query-string-machine/issues/15, https://github.com/phetsims/query-string-machine/issues/16

--- js/common/PlinkoProbabilityQueryParameters.js
+++ js/common/PlinkoProbabilityQueryParameters.js
@@ -11,6 +11,7 @@ define( function( require ) {
 
   // modules
   var plinkoProbability = require( 'PLINKO_PROBABILITY/plinkoProbability' );
+  var Util = require( 'DOT/Util' );
 
   var PlinkoProbabilityQueryParameters = QueryStringMachine.getAll( {
 
@@ -18,21 +19,30 @@ define( function( require ) {
     // Used to test overflow of bins.
     maxBallsIntro: {
       type: 'number',
-      defaultValue: 100
+      defaultValue: 100,
+      isValidValue: function( value ) {
+        return value > 0 && Util.isInteger( value );
+      }
     },
 
     // Maximum number of balls that can be in any 1 bin in the Lab screen, e.g. maxBallsLab=10
     // Use this to test the 'Out of Balls!' dialog without having to wait an eternity.
     maxBallsLab: {
       type: 'number',
-      defaultValue: 9999
+      defaultValue: 9999,
+      isValidValue: function( value ) {
+        return value > 0 && Util.isInteger( value );
+      }
     },
 
     // Number of balls to put in the Lab screen histogram at startup, e.g. populateHistogram=20
     // Use this to quickly test the histogram.
     histogramBallsLab: {
       type: 'number',
-      defaultValue: 0
+      defaultValue: 0,
+      isValidValue: function( value ) {
+        return value > 0 && Util.isInteger( value );
+      }
     },
 
     // Uses 3D appearance for the Play button, see https://github.com/phetsims/plinko-probability/issues/26
