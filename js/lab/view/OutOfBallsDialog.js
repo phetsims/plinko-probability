afa80b118e96974a7e3c2f5f180db25f2acc3a6b
jessegreenberg <jesse.greenberg@colorado.edu>
2017-07-28 16:40:49 -0600

    remove workaround for issue in Dialog that has been fixed, see phetsims/joist#346, #72

--- js/lab/view/OutOfBallsDialog.js
+++ js/lab/view/OutOfBallsDialog.js
@@ -11,7 +11,6 @@ define( function( require ) {
   // modules
   var Dialog = require( 'JOIST/Dialog' );
   var inherit = require( 'PHET_CORE/inherit' );
-  var Panel = require( 'SUN/Panel' );
   var PhetFont = require( 'SCENERY_PHET/PhetFont' );
   var plinkoProbability = require( 'PLINKO_PROBABILITY/plinkoProbability' );
   var Text = require( 'SCENERY/nodes/Text' );
@@ -26,7 +25,9 @@ define( function( require ) {
   function OutOfBallsDialog( options ) {
 
     options = _.extend( {
-      modal: true
+      modal: true,
+      xMargin: 60,
+      yMargin: 50
     }, options );
 
     var messageNode = new Text( outOfBallsString, {
@@ -34,16 +35,7 @@ define( function( require ) {
       maxWidth: 350
     } );
 
-    // WORKAROUND: An intermediate panel is needed to work around a problem that occurs when using
-    // xMargin and yMargin options for Dialog. See https://github.com/phetsims/joist/issues/346
-    var dialogContent = new Panel( messageNode, {
-      fill: null,
-      stroke: null,
-      xMargin: 40,
-      yMargin: 30
-    } );
-
-    Dialog.call( this, dialogContent, options );
+    Dialog.call( this, messageNode, options );
   }
 
   plinkoProbability.register( 'OutOfBallsDialog', OutOfBallsDialog );
