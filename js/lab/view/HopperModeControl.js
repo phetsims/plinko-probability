84204b677ef52716ee975900e675147d9792a46c
phet-dev <phetadmin@gmail.com>
2020-01-22 03:52:32 -0700

    update copyright dates from daily grunt work

--- js/lab/view/HopperModeControl.js
+++ js/lab/view/HopperModeControl.js
@@ -1,4 +1,4 @@
-// Copyright 2015-2019, University of Colorado Boulder
+// Copyright 2015-2020, University of Colorado Boulder
 
 /**
  * Scenery Node that displays three Radio Buttons that control the flow of Balls
