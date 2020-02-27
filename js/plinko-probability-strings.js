69ad1c6cf2857f7f41c99f0f7b534f38a7b5d14d
samreid <reids@colorado.edu>
2020-02-27 12:07:57 -0700

    Add images and strings, see https://github.com/phetsims/chipper/issues/875

---
+++ js/plinko-probability-strings.js
@@ -0,0 +1,14 @@
+// Copyright 2020, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+
+import getStringModule from '../../chipper/js/getStringModule.js';
+import plinkoProbability from './plinkoProbability.js';
+
+const plinkoProbabilityStrings = getStringModule( 'PLINKO_PROBABILITY' );
+
+plinkoProbability.register( 'plinkoProbabilityStrings', plinkoProbabilityStrings );
+
+export default plinkoProbabilityStrings;
