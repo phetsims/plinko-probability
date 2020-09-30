88a5540d1736b98f09edd24296afd85388bb7585
pixelzoom <cmalley@pixelzoom.com>
2020-09-29 20:16:25 -0600

    convert RadioButtonGroup subclasses to ES6 class, https://github.com/phetsims/tasks/issues/1044

--- js/common/view/HistogramModeControl.js
+++ js/common/view/HistogramModeControl.js
@@ -7,7 +7,6 @@
  * @author Denzell Barnett (Berea College)
  */
 
-import inherit from '../../../../phet-core/js/inherit.js';
 import merge from '../../../../phet-core/js/merge.js';
 import Image from '../../../../scenery/js/nodes/Image.js';
 import RadioButtonGroup from '../../../../sun/js/buttons/RadioButtonGroup.js';
@@ -16,42 +15,42 @@ import plinkoProbability from '../../plinkoProbability.js';
 // constants
 const ICON_WIDTH = 35;
 
-/**
- * @param {Property.<string>} histogramModeProperty - see PlinkoProbabilityCommonView
- * @param {string} topValue - value associated with top radio button
- * @param {HTMLImageElement} topImage - image used to create icon for top radio button
- * @param {string} bottomValue - value associated with bottom radio button
- * @param {HTMLImageElement} bottomImage - image used to create icon for bottom radio button
- * @param {Object} [options]
- * @constructor
- */
-function HistogramModeControl( histogramModeProperty, topValue, topImage, bottomValue, bottomImage, options ) {
+class HistogramModeControl extends RadioButtonGroup {
 
-  options = merge( {
-    spacing: 5, // vertical separation of the buttons
-    cornerRadius: 10,
-    baseColor: 'white',
-    buttonContentXMargin: 5,
-    buttonContentYMargin: 5,
-    selectedStroke: 'black',
-    deselectedLineWidth: 1,
-    selectedLineWidth: 2
-  }, options );
+  /**
+   * @param {Property.<string>} histogramModeProperty - see PlinkoProbabilityCommonView
+   * @param {string} topValue - value associated with top radio button
+   * @param {HTMLImageElement} topImage - image used to create icon for top radio button
+   * @param {string} bottomValue - value associated with bottom radio button
+   * @param {HTMLImageElement} bottomImage - image used to create icon for bottom radio button
+   * @param {Object} [options]
+   */
+  constructor( histogramModeProperty, topValue, topImage, bottomValue, bottomImage, options ) {
 
-  // create the icons for the radio buttons
-  const topNode = new Image( topImage );
-  topNode.scale( ICON_WIDTH / topNode.width );
+    options = merge( {
+      spacing: 5, // vertical separation of the buttons
+      cornerRadius: 10,
+      baseColor: 'white',
+      buttonContentXMargin: 5,
+      buttonContentYMargin: 5,
+      selectedStroke: 'black',
+      deselectedLineWidth: 1,
+      selectedLineWidth: 2
+    }, options );
 
-  const bottomNode = new Image( bottomImage );
-  bottomNode.scale( ICON_WIDTH / bottomNode.width );
+    // create the icons for the radio buttons
+    const topNode = new Image( topImage );
+    topNode.scale( ICON_WIDTH / topNode.width );
 
-  RadioButtonGroup.call( this, histogramModeProperty, [
-    { value: topValue, node: topNode },
-    { value: bottomValue, node: bottomNode }
-  ], options );
+    const bottomNode = new Image( bottomImage );
+    bottomNode.scale( ICON_WIDTH / bottomNode.width );
+
+    super( histogramModeProperty, [
+      { value: topValue, node: topNode },
+      { value: bottomValue, node: bottomNode }
+    ], options );
+  }
 }
 
 plinkoProbability.register( 'HistogramModeControl', HistogramModeControl );
-
-inherit( RadioButtonGroup, HistogramModeControl );
 export default HistogramModeControl;
