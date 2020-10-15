19bbdb2913efacf124e2171a1380ae933873595e
pixelzoom <cmalley@pixelzoom.com>
2020-10-15 10:03:53 -0600

    convert ShadedSphereNode to ES6 class, https://github.com/phetsims/tasks/issues/1044

--- js/common/view/BallNode.js
+++ js/common/view/BallNode.js
@@ -6,23 +6,22 @@
  * @author Martin Veillette (Berea College)
  */
 
-import inherit from '../../../../phet-core/js/inherit.js';
 import ShadedSphereNode from '../../../../scenery-phet/js/ShadedSphereNode.js';
 import plinkoProbability from '../../plinkoProbability.js';
 import PlinkoProbabilityConstants from '../PlinkoProbabilityConstants.js';
 
-/**
- * @param {number} radius - in view coordinates
- * @constructor
- */
-function BallNode( radius ) {
-  ShadedSphereNode.call( this, 2 * radius, {
-    mainColor: PlinkoProbabilityConstants.BALL_COLOR,
-    highlightColor: PlinkoProbabilityConstants.BALL_HIGHLIGHT_COLOR
-  } );
+class BallNode extends ShadedSphereNode {
+
+  /**
+   * @param {number} radius - in view coordinates
+   */
+  constructor( radius ) {
+    super( 2 * radius, {
+      mainColor: PlinkoProbabilityConstants.BALL_COLOR,
+      highlightColor: PlinkoProbabilityConstants.BALL_HIGHLIGHT_COLOR
+    } );
+  }
 }
 
 plinkoProbability.register( 'BallNode', BallNode );
-
-inherit( ShadedSphereNode, BallNode );
 export default BallNode;
