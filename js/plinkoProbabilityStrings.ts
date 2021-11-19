6eb20fdafc0977e80535e42669b939a31ebc93e3
Sam Reid <reids@colorado.edu>
2021-11-18 17:43:42 -0700

    Run grunt update, see https://github.com/phetsims/chipper/issues/1148

---
+++ js/plinkoProbabilityStrings.ts
@@ -0,0 +1,40 @@
+// Copyright 2020-2021, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+/* eslint-disable */
+import getStringModule from '../../chipper/js/getStringModule.js';
+import plinkoProbability from './plinkoProbability.js';
+
+type StringsType = {
+  'plinko-probability': {
+    'title': string
+  },
+  'screen': {
+    'lab': string,
+    'intro': string
+  },
+  'fraction': string,
+  'count': string,
+  'bin': string,
+  'rows': string,
+  'binaryProbability': string,
+  'n': string,
+  's': string,
+  'sMean': string,
+  'ball': string,
+  'path': string,
+  'none': string,
+  'ideal': string,
+  'outOfBalls': string,
+  'mu': string,
+  'sigma': string,
+  'xBar': string
+};
+
+const plinkoProbabilityStrings = getStringModule( 'PLINKO_PROBABILITY' ) as StringsType;
+
+plinkoProbability.register( 'plinkoProbabilityStrings', plinkoProbabilityStrings );
+
+export default plinkoProbabilityStrings;
