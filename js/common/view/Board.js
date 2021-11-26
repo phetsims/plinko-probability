5b9b336ad7a75fdc9864992bac8a248551465913
phet-dev <phetadmin@gmail.com>
2021-11-26 03:20:25 -0700

    update copyright dates from daily grunt work

--- js/common/view/Board.js
+++ js/common/view/Board.js
@@ -1,4 +1,4 @@
-// Copyright 2015-2020, University of Colorado Boulder
+// Copyright 2015-2021, University of Colorado Boulder
 
 /**
  * A Scenery node that depicts a wooden triangular board.
--- js/common/view/PegsNode.js
+++ js/common/view/PegsNode.js
@@ -1,4 +1,4 @@
-// Copyright 2002-2020, University of Colorado Boulder
+// Copyright 2002-2021, University of Colorado Boulder
 
 /**
  * Renders a triangular lattice of pegs for a Galton board.
