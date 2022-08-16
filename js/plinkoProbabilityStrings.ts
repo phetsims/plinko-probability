0d414dda1ca9797f648da5c1ffb807850c592c11
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-15 20:36:36 -0600

    Grunt update, and changes for string Properties, see https://github.com/phetsims/chipper/issues/1302

--- js/plinkoProbabilityStrings.ts
+++ js/plinkoProbabilityStrings.ts
@@ -5,32 +5,52 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
+import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
 import plinkoProbability from './plinkoProbability.js';
 
 type StringsType = {
   'plinko-probability': {
     'title': string;
+    'titleProperty': TReadOnlyProperty<string>;
   };
   'screen': {
     'lab': string;
+    'labProperty': TReadOnlyProperty<string>;
     'intro': string;
+    'introProperty': TReadOnlyProperty<string>;
   };
   'fraction': string;
+  'fractionProperty': TReadOnlyProperty<string>;
   'count': string;
+  'countProperty': TReadOnlyProperty<string>;
   'bin': string;
+  'binProperty': TReadOnlyProperty<string>;
   'rows': string;
+  'rowsProperty': TReadOnlyProperty<string>;
   'binaryProbability': string;
+  'binaryProbabilityProperty': TReadOnlyProperty<string>;
   'n': string;
+  'nProperty': TReadOnlyProperty<string>;
   's': string;
+  'sProperty': TReadOnlyProperty<string>;
   'sMean': string;
+  'sMeanProperty': TReadOnlyProperty<string>;
   'ball': string;
+  'ballProperty': TReadOnlyProperty<string>;
   'path': string;
+  'pathProperty': TReadOnlyProperty<string>;
   'none': string;
+  'noneProperty': TReadOnlyProperty<string>;
   'ideal': string;
+  'idealProperty': TReadOnlyProperty<string>;
   'outOfBalls': string;
+  'outOfBallsProperty': TReadOnlyProperty<string>;
   'mu': string;
+  'muProperty': TReadOnlyProperty<string>;
   'sigma': string;
+  'sigmaProperty': TReadOnlyProperty<string>;
   'xBar': string;
+  'xBarProperty': TReadOnlyProperty<string>;
 };
 
 const plinkoProbabilityStrings = getStringModule( 'PLINKO_PROBABILITY' ) as StringsType;
