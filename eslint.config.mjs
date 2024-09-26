8cabf3aa9d7f09786a9e342614d07ebe64bb2c22
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-09-26 14:09:46 -0600

    typo, https://github.com/phetsims/chipper/issues/1474
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 // Copyright 2024, University of Colorado Boulder
 
 /**
- * ESlint configuration for plinko-probability.
+ * ESLint configuration for plinko-probability.
  *
  * @author Sam Reid (PhET Interactive Simulations)
  * @author Michael Kauzmann (PhET Interactive Simulations)
